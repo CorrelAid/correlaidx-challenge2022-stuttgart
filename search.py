89915b4abf07600638d312cf9203e06585d536aa
Philipp Wussow <64583398+ShirkahnW@users.noreply.github.com>
2022-11-28 19:22:34 +0100

    First Commit of Source Code

---
+++ search.py
@@ -0,0 +1,15 @@
+from database import Neo4jConnection
+
+conn = Neo4jConnection(uri="bolt://localhost:7687", user="test", pwd="test")
+
+def find_officer(name: str, limit: int):
+    query_string = f"""
+    MATCH (a:Officer)
+    WHERE a.name CONTAINS "{name}"
+    RETURN a
+    LIMIT {limit}
+    """
+    result = conn.query(query_string)
+    return result
+
+print(find_officer("Michael Sauer",1))
